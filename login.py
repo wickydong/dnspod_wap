ccc7b5b74898e6a443b93d05377551a9b71959a8
Kun Dong <kdong@dnspod.com>
2013-07-15 11:25:37 +0800

    modified session from current

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 # -*-coding:utf-8-*-
 import json
 import requests
-from flask import Flask,render_template,request,current_app,session,url_for,redirect
+from flask import Flask,render_template,request,session,url_for,redirect,session
 
 app = Flask(__name__)
 
@@ -17,19 +17,26 @@ def index():
 
 @app.route("/login", methods=["POST"])
 def login():
-    app.user_mail = request.form["user_mail"]
-    app.user_passwd = request.form["user_passwd"]
-    login_data = {"login_email": app.user_mail,"login_password": app.user_passwd,"format": "json"}
+    user_mail = request.form["user_mail"]
+    user_passwd = request.form["user_passwd"]
+    login_data = {"login_email": user_mail,"login_password": user_passwd,"format": "json"}
     login_request = requests.post("https://dnsapi.cn/User.Detail",data=login_data)
-    print login_request.text
     user_message = json.loads(login_request.text)
-    return redirect(url_for('domainlist'))
+    user_status = user_message["status"]
+    user_code = user_status["code"]
+    if int(user_code) == 1:
+        session["user_mail"] = user_mail
+        session["user_passwd"] = user_passwd
+        print session["user_mail"]
+        return redirect(url_for('domainlist'))
+    else:
+        return render_template("index.html")
 
 #进入域名列表
 
 @app.route("/domainlist")
 def domainlist(domainfree=None,domainvip=None):
-    login_data = {"login_email": current_app.user_mail,"login_password": current_app.user_passwd,"format": "json"}
+    login_data = {"login_email": session["user_mail"],"login_password": session["user_passwd"],"format": "json"}
     login_request = requests.post("https://dnsapi.cn/Domain.List",data=login_data)
     domainlist = json.loads(login_request.text)
     domaininfo = domainlist["info"]
@@ -59,7 +66,7 @@ def add_domain():
 @app.route("/add_domains",methods=["POST","GET"])
 def Add_Domains(add_domain_status=None):
     add_domain = request.form["add_domain"]
-    add_domain_data = {"login_email": current_app.user_mail,"login_password": current_app.user_passwd,"domain":add_domain,"format":"json"} 
+    add_domain_data = {"login_email": session["user_mail"],"login_password": session["user_passwd"],"domain":add_domain,"format":"json"} 
     add_domain_request = requests.post("https://dnsapi.cn/Domain.Create",data=add_domain_data)
     add_domain_result = json.loads(add_domain_request.text)
     status = add_domain_result['status']
@@ -72,6 +79,8 @@ def Add_Domains(add_domain_status=None):
     else:
         add_domain_status = "fail"
     return render_template("adddomain.html", add_domain_status=add_domain_status)
-    
-if __name__ == "__main__":
-   app.run(host="0.0.0.0",port=1234,debug=True)    
+
+
+if __name__ == "__main__":   
+    app.secret_key = 'fuckmjj'
+    app.run(host="0.0.0.0",port=1234,debug=True)    
